0f9d649d227d839d183f4d6da4b1d96c59573798
Scott Motte <mot@mot.la>
2023-05-05 21:56:04 -0700

    Bump to v1.21.0

--- Formula/dotenv-vault-node.rb
+++ Formula/dotenv-vault-node.rb
@@ -1,9 +1,9 @@
 class DotenvVaultNode < Formula
   desc "node.js dependency for dotenv-vault"
   homepage "https://dotenv.org"
-  url "https://nodejs.org/download/release/v15.14.0/node-v15.14.0-darwin-x64.tar.xz"
-  version "15.14.0"
-  sha256 "f355aeda5049fdbac4acca23a7bb6f66e145a179a52bd2489e3f76fbe0feb161"
+  url "https://nodejs.org/download/release/v16.15.1/node-v16.15.1-darwin-x64.tar.xz"
+  version "16.15.1"
+  sha256 "bdad616d4d388364a7734cf61d078912c468809ee54c2869edcd813c472ebe3a"
   keg_only "dotenv-vault-node is only used by dotenv-vault (dotenv-org/brew/dotenv-vault)"
 
   def install
