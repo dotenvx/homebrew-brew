2d032173ca63048b1264be3ae4d2f2f60ba88002
motdotenv <mot@dotenv.org>
2023-11-19 18:03:20 +0000

    "Brew formula update for dotenv version v0.2.0"

--- Formula/dotenv.rb
+++ Formula/dotenv.rb
@@ -5,36 +5,36 @@
 class Dotenv < Formula
   desc "“dotenv made better“ from the creator of `dotenv`"
   homepage "https://github.com/dotenv-org/dotenv"
-  url "https://github.com/dotenv-org/dotenv/archive/refs/tags/v0.1.18.tar.gz"
-  sha256 "d096359cee9dba7093b8a2d5bff76c1f0fa64e17c97c6424df62e3af4eddcc1f"
+  url "https://github.com/dotenv-org/dotenv/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "98ba11a90bac4a9e8a27535a1b9d5525164369f0f9918cfde379d635ba593e90"
 
   on_macos do
     on_intel do
-      url "https://github.com/dotenv-org/dotenv/releases/download/v0.1.18/git-dotenv-0.1.18-darwin-amd64.tar.gz"
-      sha256 "69a4789e84b313a3f1c30dcf3e438dcb95422af0ca23eb9eebb5aa9855ad5682"
+      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.0/dotenv-0.2.0-darwin-amd64.tar.gz"
+      sha256 "e00f569f4efe27f9b82376e0bdc77b49562ebe7f74fd649a34ed4c530d1d7122"
     end
 
     on_arm do
-      url "https://github.com/dotenv-org/dotenv/releases/download/v0.1.18/git-dotenv-0.1.18-darwin-arm64.tar.gz"
-      sha256 "2c1f14caafcae3282faed37709e390fd0588e9fd938be578ce4edc24e5990b37"
+      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.0/dotenv-0.2.0-darwin-arm64.tar.gz"
+      sha256 "797adf6b36c7d84535a489b8f132bde96b3489f311456f70dd43803918761977"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/dotenv-org/dotenv/releases/download/v0.1.18/git-dotenv-0.1.18-linux-amd64.tar.gz"
-      sha256 "310b906b8826300af32ac4429d8f405c07d0c1c2557ce0fc6d871fdaf15292da"
+      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.0/dotenv-0.2.0-linux-amd64.tar.gz"
+      sha256 "c4d1cf60b5d868b7a5165f5f962482eb3491fa64b8401690f6ef3278ca97b601"
     end
 
     on_arm do
-      url "https://github.com/dotenv-org/dotenv/releases/download/v0.1.18/git-dotenv-0.1.18-linux-arm64.tar.gz"
-      sha256 "95d4f2a55c29b29500cf6f17fe5ca094da67932fd2f82fdd5a51591abd084f34"
+      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.0/dotenv-0.2.0-linux-arm64.tar.gz"
+      sha256 "cfa016424448c311b7f59eba0546a593508ce566e58d6279a363cf81be350f56"
     end
   end
 
   def install
     bin.install "dotenv"
-    bin.install_symlink "dotenv" => "git-dotenv"
+bin.install_symlink "dotenv" => "git-dotenv"
   end
 
   test do
