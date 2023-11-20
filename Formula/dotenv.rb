c05dccde9dcf741ba991386a9d2c2907728b2706
motdotenv <mot@dotenv.org>
2023-11-20 18:41:58 +0000

    "Brew formula update for dotenv version v0.2.15"

--- Formula/dotenv.rb
+++ Formula/dotenv.rb
@@ -5,30 +5,30 @@
 class Dotenv < Formula
   desc "Better dotenv–from the creator of `dotenv`"
   homepage "https://github.com/dotenv-org/dotenv"
-  url "https://github.com/dotenv-org/dotenv/archive/refs/tags/v0.2.14.tar.gz"
-  sha256 "23fe5b7384cc5ec59605a2cc4d2412af4eb1f20427c65447de9e90600e479ec1"
+  url "https://github.com/dotenv-org/dotenv/archive/refs/tags/v0.2.15.tar.gz"
+  sha256 "e03b221d201ed1bcb410e26664b320232683926e9759804f2801558f1086cfbf"
 
   on_macos do
     on_intel do
-      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.14/dotenv-0.2.14-darwin-amd64.tar.gz"
-      sha256 "a908aca683ac4ea4738671501af27c240cbccda61898f91401790b1834b062c6"
+      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.15/dotenv-0.2.15-darwin-amd64.tar.gz"
+      sha256 "6f7d983643a1eb80d89badfedfee3976fd464ed1d3e92d43b01b160acfb7bde4"
     end
 
     on_arm do
-      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.14/dotenv-0.2.14-darwin-arm64.tar.gz"
-      sha256 "40f65756a12c4f6a7bc189e50713edac24c9b0a3e1c6650eb79b95a3a3537586"
+      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.15/dotenv-0.2.15-darwin-arm64.tar.gz"
+      sha256 "1513f1e0ec9bb5a438434a2084161b8c2cd52602f42571cc56d92d4fb91fd95b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.14/dotenv-0.2.14-linux-amd64.tar.gz"
-      sha256 "7ec6795859badbe2de3cdc0683094d6d759e283db9ac4ec93920aa9899b7e0ef"
+      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.15/dotenv-0.2.15-linux-amd64.tar.gz"
+      sha256 "ba81aa31bd10ec662326e45105a8d6f366809bb045ce247eba09ecfd8742b27c"
     end
 
     on_arm do
-      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.14/dotenv-0.2.14-linux-arm64.tar.gz"
-      sha256 "9b8db23bfac5af01ca69837efc1e5eca5a96cc87776eaecd4fadae7eee896347"
+      url "https://github.com/dotenv-org/dotenv/releases/download/v0.2.15/dotenv-0.2.15-linux-arm64.tar.gz"
+      sha256 "c338d01a965c51a22161f0d3e2a5d472fb1a3ca64d8a6c0c357167d64c8891c9"
     end
   end
 
