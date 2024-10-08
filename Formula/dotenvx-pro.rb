7b1ea85206b8e02689efb0a7f773d86bd0c4e1cb
motdotenv <mot@dotenv.org>
2024-10-08 02:26:55 +0000

    chore: brew formula update for dotenvx-pro v0.12.2

--- Formula/dotenvx-pro.rb
+++ Formula/dotenvx-pro.rb
@@ -5,30 +5,30 @@
 class DotenvxPro < Formula
   desc "Dotenvx-pro cli"
   homepage "https://github.com/dotenvx/dotenvx-pro"
-  url "https://github.com/dotenvx/dotenvx-pro/archive/refs/tags/v0.12.1.tar.gz"
-  sha256 "a27758235693a6a818d03fa973ff368c842ce3b2da73bd5c80321a4d3347cd7c"
+  url "https://github.com/dotenvx/dotenvx-pro/archive/refs/tags/v0.12.2.tar.gz"
+  sha256 "d2b7779590b47a53ba80d5ea3ea13c198df93884e63890bc7bc0f92ddb88c818"
 
   on_macos do
     on_intel do
-      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.12.1/dotenvx-pro-0.12.1-darwin-amd64.tar.gz"
-      sha256 "7c115489208dbceb99a7a36f69a51dbf6cfe5d4af4e27153c679625a93ae5a39"
+      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.12.2/dotenvx-pro-0.12.2-darwin-amd64.tar.gz"
+      sha256 "551f2472b9011df99ac24f5395b9be7b9c96c87e2319a013762572f75aaa192d"
     end
 
     on_arm do
-      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.12.1/dotenvx-pro-0.12.1-darwin-arm64.tar.gz"
-      sha256 "6cde71e78608bb55fd12a82c9edf77c235ab171792b968a7af87fc998c7096cf"
+      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.12.2/dotenvx-pro-0.12.2-darwin-arm64.tar.gz"
+      sha256 "18d885803eea6373e128391d1529354e2836e2668da4073e6df49d5a8be0d04d"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.12.1/dotenvx-pro-0.12.1-linux-amd64.tar.gz"
-      sha256 "ac3764dd6815f98374a7c804fb3530be99ddccff14040d55de73204ab46c8bf4"
+      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.12.2/dotenvx-pro-0.12.2-linux-amd64.tar.gz"
+      sha256 "7218fba823bd99edc9a545671f531f4f826f17a16b0eb444fab3308d2b2023c9"
     end
 
     on_arm do
-      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.12.1/dotenvx-pro-0.12.1-linux-arm64.tar.gz"
-      sha256 "be19736341def3fd1c665eac8a37c71ffb0dc0cfc5e31a8d852dc07256c7e8d7"
+      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.12.2/dotenvx-pro-0.12.2-linux-arm64.tar.gz"
+      sha256 "9d3647ccab70e74c567926f9fe62128a3daa2c01d50bf3c4e8a734261d11a9ac"
     end
   end
 
