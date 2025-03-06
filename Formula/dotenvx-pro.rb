4105f254f5c9668491a76289e96d94f23a080d83
motdotenv <mot@dotenv.org>
2025-03-06 19:03:46 +0000

    chore: brew formula update for dotenvx-pro v0.16.0

--- Formula/dotenvx-pro.rb
+++ Formula/dotenvx-pro.rb
@@ -5,37 +5,37 @@
 class DotenvxPro < Formula
   desc "Dotenvx pro 🏆 – for teams and organizations that need secure, scalable secrets m"
   homepage "https://github.com/dotenvx/dotenvx-pro"
-  url "https://github.com/dotenvx/dotenvx-pro/archive/refs/tags/v0.15.1.tar.gz"
-  sha256 "491c47a025f5c6f8437501675ef6f59cd8f166d4f72f4aef60e1138f079ee430"
+  url "https://github.com/dotenvx/dotenvx-pro/archive/refs/tags/v0.16.0.tar.gz"
+  sha256 "5e86e52e15a5ace43670c8f3e6130df6662ce2b87bfd6ca935eb1f0ed7d2bf9e"
+
+  depends_on "dotenvx"
 
   on_macos do
     on_intel do
-      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.15.1/dotenvx-pro-0.15.1-darwin-amd64.tar.gz"
-      sha256 "df3fd2181369c2ea068f378f3d31d9117176197483f2f7ee3a7ef5447ccdf810"
+      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.16.0/dotenvx-pro-0.16.0-darwin-amd64.tar.gz"
+      sha256 "49b31c433fabe5ed8a3897f95b3c20caf24fbbc2463a5d433b96f599319dd170"
     end
 
     on_arm do
-      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.15.1/dotenvx-pro-0.15.1-darwin-arm64.tar.gz"
-      sha256 "d9afbe912ed2929cb3f21af71f05e701824e80a22b831745b7c03ce5e5c0a06a"
+      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.16.0/dotenvx-pro-0.16.0-darwin-arm64.tar.gz"
+      sha256 "c5a9e69288cf30ab6cb5b12a2d0b4fc6ae39c6f9b5144d52f6ea640db1b5e273"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.15.1/dotenvx-pro-0.15.1-linux-amd64.tar.gz"
-      sha256 "5d594adc5036950b9e4cdca7724bfc4fe62607e5e687b646f8fa8667f78533c2"
+      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.16.0/dotenvx-pro-0.16.0-linux-amd64.tar.gz"
+      sha256 "21851b0b489211aa1415bfcd1199bc2c614db3ffbe7ba7e2a28014bd19a7da77"
     end
 
     on_arm do
-      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.15.1/dotenvx-pro-0.15.1-linux-arm64.tar.gz"
-      sha256 "1792fc61592c08786776aa4dbfa96140fc2504fbde690269afd8762e2c31060e"
+      url "https://github.com/dotenvx/dotenvx-pro/releases/download/v0.16.0/dotenvx-pro-0.16.0-linux-arm64.tar.gz"
+      sha256 "7b63e35b2823f28b1b00d43357e5e01f218b166056f6906799d88a33d2382cd2"
     end
   end
 
   def install
-    system "brew", "tap", "dotenvx/brew" unless `brew tap`.include?("dotenvx/brew")
-system "brew", "install", "dotenvx/brew/dotenvx" unless Formula["dotenvx"].installed?
-bin.install "dotenvx-pro"
+    bin.install "dotenvx-pro";
   end
 
   test do
